00c73f64866c83b1393753db0ca7b2d524ea7cd0
Data88 <andrew1on1@gmail.com>
2023-08-11 07:48:49 +0200

    fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
+	site: 'https://look.capetown/', // Write here your website url
 	experimental: {
 		assets: true
 		// viewTransitions: false,
--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -8,10 +8,10 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG',
-	title: 'Astro Theme OpenBlog',
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+	author: 'Andrew',
+	title: 'LOOK',
+	description: 'Artist Management in Cape Town.',
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
-	shareMessage: 'Share this post' // Message to share a post on social media
+	shareMessage: 'Share this artist' // Message to share a post on social media
 }
